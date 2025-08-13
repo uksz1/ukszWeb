e00c7a99dc4f5c967fa13d0081094efbe556c6f5
uksz1 <uksz1@proton.me>
2025-08-13 20:40:05 +0200

    1.0.22 hotfix?

--- webring-widget.js
+++ webring-widget.js
@@ -8,7 +8,7 @@
   "error": "https://oerrorpage.neocities.org",
   "yoyledotcity": "https://yoyle.city",
   "YEESITE": "https://yeepsite.nekoweb.org",
-  "ockolus": "https://ockolus.neocities.org/"
+  "ockolus": "https://ockolus.neocities.org"
   };
 
   const baseUrl = location.origin;
