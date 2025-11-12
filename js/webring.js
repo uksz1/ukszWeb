ddaeab9681eda2d5109444b63509d8dcbf952aec
uksz1 <uksz1@proton.me>
2025-11-12 14:11:25 +0100

    i forgot

--- js/webring.js
+++ js/webring.js
@@ -1,19 +1,28 @@
 (async () => {
-  const [rawSlug, directionRaw] = window.location.pathname.split('/').filter(Boolean);
-  const slug = rawSlug.replace(/^@/, '');
-  const direction = directionRaw.replace(/\.html$/, '');
-  const res = await fetch(`/webring.json?${Date.now()}`);
-  if (!res.ok) return;
-  const mapping = await res.json();
-  const slugs = Object.keys(mapping);
-  const index = slugs.indexOf(slug);
-  if (index === -1) return;
-  const newIndex = direction === 'next'
-    ? (index + 1) % slugs.length
-    : direction === 'prev'
-      ? (index - 1 + slugs.length) % slugs.length
-      : -1;
-  if (newIndex === -1) return;
-  const targetUrl = mapping[slugs[newIndex]];
-  if (targetUrl) window.location.href = targetUrl;
+  const response = await fetch(`https://web.jae.gg/webring.json?${Date.now()}`);
+  const webring = await response.json();
+  const baseUrl = location.origin;
+  const widget = document.getElementById(`JaeWEB`);
+  if(!widget) return;
+  Object.assign(widget.style,{
+    display: `inline-block`,
+    height: `31px`,
+    width: `150px`,
+    fontFamily: `monospace`,
+    fontSize: `10px`
+  })
+    let slug;
+    for(const [name, url] of Object.entries(webring)){
+      if(url === baseUrl){
+        slug = name;
+        break;
+      }
+    }
+    if(!slug){
+      widget.innerHTML = `<div style="width:62px;height:31px;float:left;display:flex;text-align:center;align-items: center;"><span style="width:100%;user-select:none;">waiting<br>to join</span></div><div style="width:88px;height:31px;display:inline"><a href="https://web.jae.gg" target="_blank"><img src="https://web.jae.gg/images/JaeWEB.gif" alt="JaeWEB"></a></div>`;
+      widget.style.backgroundColor=`#7133AB80`;
+      widget.style.borderRadius=`4px`;
+      return;
+    }
+    widget.innerHTML = `<a href="https://web.jae.gg/r?${slug}&prev"><img src="https://web.jae.gg/images/prev.gif" alt="previous"></a><a href="https://web.jae.gg" target="_blank" rel="noopener"><img src="https://web.jae.gg/images/JaeWEB.gif" alt="web.jae.gg webring"></a><a href="https://web.jae.gg/r?${slug}&next"><img src="https://web.jae.gg/images/next.gif" alt="next"></a>`
 })();
